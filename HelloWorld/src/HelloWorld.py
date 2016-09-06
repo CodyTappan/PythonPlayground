baf698e14fd7f6aa28b00b6dbf367b17f72076a9
Cody Tappan <codytappan@gmail.com>
2016-09-05 19:05:56 -0700

    Moving user greeting functionality to separate file

--- HelloWorld/src/HelloWorld.py
+++ HelloWorld/src/HelloWorld.py
@@ -1,10 +1,10 @@
+import greetingPrinter
+
 # Hello World!
 
 print("\nHello, World!\n")
 
-name = input("And what is your name good sir or madam? ")
-
-print("Hi,", name, "\n")
+greetingPrinter.greetUser();
 
 userInput = input("-- Press Enter to Quit --")
 
---
+++ HelloWorld/src/greetingPrinter.py
@@ -0,0 +1,6 @@
+# Prints a greeting for Hello World!
+
+# Greet the user!
+def greetUser():
+	userName = input("\nWhat is your name, good sir or madam? ")
+	print("\nGreetings and well met,", userName + "!\n")
